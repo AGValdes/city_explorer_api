7394f48441bc2c08b3760a61d5a4c5e128fbcc03
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-11-11 16:30:55 -0800

    added a database

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const app = express();
-
 dotenv.config();
+const app = express();
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL)
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,28 +26,43 @@ app.get('/trails', handleTrails);
 
 function handleLocation(req, res) {
 
-  if (req.query.city !== ''){
+  if (req.query.city !== '') {
     try {
       let city = req.query.city;
       let url = `http://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
-      let locations = {};
+      let existingSQLData = 'SELECT * FROM cityLocation WHERE search_query = $1';
 
-      if (locations[url]){
-        res.send(locations[url]);
-      } else {
-        superagent.get(url)
-          .then (data => {
-            const geoData = data.body[0];
-            const location = new Location (city, geoData);
-            locations[url]=location;
 
-            res.json(location);
-          })
-      }
+      client.query(existingSQLData, [city])
+        .then(data => {
+          if (data.rows.length > 0) {
+            // console.log('cityLocationsDatabase')
+            res.json(data.rows[0]);
+          } else {
+            // console.log('not saved yet');
+            superagent.get(url)
+              .then(data => {
+                const geoData = data.body[0];
+                const location = new Location(city, geoData);
+
+
+                let SQL = 'INSERT INTO cityLocation (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING *';
+                let values = [city, geoData.display_name, geoData.lat, geoData.lon];
+                client.query(SQL, values)
+                  .then(() => {
+                    res.json(location);
+                  });
+              });
+
+
+          }
+        })
+
     } catch (error) {
       console.error(error);
     }
-  } else {
+  }
+  else {
     res.status(500).send('Sorry, something went wrong');
   }
 
@@ -53,20 +70,20 @@ function handleLocation(req, res) {
 
 function handleWeather(req, res) {
 
-  if (req.query.city !== ''){
+  if (req.query.city !== '') {
     try {
-      let cityLatitude= req.query.latitude;
-      let cityLongitude= req.query.longitude;
+      let cityLatitude = req.query.latitude;
+      let cityLongitude = req.query.longitude;
       let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${cityLatitude}&lon=${cityLongitude}&format=json&days=8`;
       let weather = {};
 
-      if (weather[url]){
+      if (weather[url]) {
         res.send(weather[url]);
       } else {
         superagent.get(url)
-          .then (dataFromAPI => {
+          .then(dataFromAPI => {
             const weatherData = dataFromAPI.body;
-            let eachDayArray = weatherData.data.map( data =>{
+            let eachDayArray = weatherData.data.map(data => {
               return new Weather(data);
             });
             weather[url] = eachDayArray;
@@ -84,23 +101,23 @@ function handleWeather(req, res) {
 
 }
 
-function handleTrails (req, res) {
+function handleTrails(req, res) {
 
-  if (req.query.city !== ''){
+  if (req.query.city !== '') {
     try {
-      let cityLatitude= req.query.latitude;
-      let cityLongitude= req.query.longitude;
+      let cityLatitude = req.query.latitude;
+      let cityLongitude = req.query.longitude;
       let url = `https://www.hikingproject.com/data/get-trails?key=${HIKING_API_KEY}&lat=${cityLatitude}&lon=${cityLongitude}&format=json`;
 
       let hiking = {};
 
-      if (hiking[url]){
+      if (hiking[url]) {
         res.send(hiking[url]);
       } else {
         superagent.get(url)
-          .then (dataFromAPI => {
+          .then(dataFromAPI => {
             const hikeData = dataFromAPI.body;
-            let eachHikeArray = hikeData.trails.map( data =>{
+            let eachHikeArray = hikeData.trails.map(data => {
               return new Hike(data);
             });
             hiking[url] = eachHikeArray;
@@ -147,7 +164,11 @@ app.use('*', (request, response) => {
   response.status(404).send('sorry, not found!');
 });
 
-app.listen(PORT, () => {
-  console.log(`server up: ${PORT}`);
-});
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server up: ${PORT}`);
+    });
+  })
+  .catch(err => console.log(err));
 
